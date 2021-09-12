c80bb6f00cfc022f2deada6bc7d25a8cf45f240f
naomis17 <90564122+naomis17@users.noreply.github.com>
2021-09-12 13:26:37 -0400

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        var scanner = new java.util.Scanner(System. in);
+        System.out.print("What is your name?");
+        var name = scanner.nextLine();
+        System.out.println("Hello,"   + name  +  ", nice to meet you!  ");
+    }
+}
